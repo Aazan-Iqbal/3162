cf6aec13e6057ed9e80fc63df1a7a17358387759
Aazan-Iqbal <2020152848@ub.edu.bz>
2023-03-15 22:43:45 -0600

    added some endpoints and handlers

--- quiz-2/recsystem/cmd/web/handlers.go
+++ quiz-2/recsystem/cmd/web/handlers.go
@@ -7,17 +7,16 @@ import (
 
 // include --about
 // include --home
-// create handler for greeting
-func (app *application) Greeting(w http.ResponseWriter, r *http.Request) {
 
-	RenderTemplates(w, "./static/html/poll.page.tmpl")
-	//RenderTemplate(w, "home.page.tmpl", nil)
-	// w.Write([]byte("Welcome to my page."))
-	//question, err := app.question.Get()
-	//if err != nil {
-	//return
-	//}
-	//w.Write([]byte(question.Body))
+// create handler for home
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+
+	} // w.Write([]byte("Welcome to my home page."))
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
 }
 
 // create handler for about
@@ -28,37 +27,93 @@ func (app *application) About(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello\n"))
 }
 
-// create handler for home
-func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+// create handler for login
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 
-	} // w.Write([]byte("Welcome to my home page."))
-	//helpers.RenderTemplates(w, "./static/html/home.page.tmpl")
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
 
 }
 
-// create handler for home
-func (app *application) MessageCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("ALLOW", "POST") //setting header in order to do a 'write'
-		//w.WriteHeader(405) //write in header
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		//w.Write([]byte("method not allowed")) //this is writing in the body
-		return
-	}
-	// w.Write([]byte("method created..."))
-	// get the form data
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
-	the_question := r.PostForm.Get("new_question")
-	_, err = app.user.Insert(the_question)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
+// create handler for LoginSubmit
+func (app *application) LoginSubmit(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignInSubmit
+func (app *application) SignInSubmit(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for ScanQrCode
+func (app *application) ScanQrCode(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for ScanQrCodeSubmit
+func (app *application) ScanQrCodeSubmit(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
+}
+
+// create handler for SignIn
+func (app *application) SignIn(w http.ResponseWriter, r *http.Request) {
+
+	RenderTemplates(w, "./ui/static/html/home.page.tmpl")
+
 }
