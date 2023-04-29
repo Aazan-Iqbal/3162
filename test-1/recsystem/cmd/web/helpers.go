6eb33b3e71b0dc2af7215b56c236219a1821880e
Aazan-Iqbal <2020152848@ub.edu.bz>
2023-04-28 23:50:35 -0600

    fixed issue with webpages not loading

--- test-1/recsystem/cmd/web/helpers.go
+++ test-1/recsystem/cmd/web/helpers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,49 +8,20 @@ import (
 	"github.com/Aazan-Iqbal/3161/quiz-2/recsystem/internal/models"
 )
 
-// cache will hold the rendered templates so
-// that we do not have to go to disk every time
-var cache = map[string]*template.Template{}
-
-func RenderTemplate(w http.ResponseWriter, tmpl string, data *templateData) {
-	// Check if the cache has the template for the passed in
-	// template string. If it does not, then we call the
-	// createTemplateCache() function to parse the file and
-	// place it into the cache
-	_, ok := cache[tmpl]
-	if !ok {
-		err := createTemplateCache(tmpl)
-		if err != nil {
-			log.Print(err.Error())
-			http.Error(w, "Internal Server Error", 500)
-		}
-	}
-	// The template is in the cache, so read it and
-	// execute it
-	ts := cache[tmpl]
-	err := ts.Execute(w, data)
-
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	//w.Write([]byte("Welcome to Polly!"))
+	ts, err := template.ParseFiles(tmpl)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
-}
-
-// The createTemplateCache() function accepts a string
-// representing a template. The template is then parsed
-// and place into a template cache. An error is returned
-func createTemplateCache(tmpl string) error {
-	templates := []string{
-		fmt.Sprintf("./ui/html/%s", tmpl),
-		"./ui/html/base.layout.tmpl",
-	}
-	ts, err := template.ParseFiles(templates...)
+	err = ts.Execute(w, nil)
 	if err != nil {
-		return err
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+
 	}
-	// Add the template to the map
-	cache[tmpl] = ts
-	return nil
 }
 
 func equipmentData(equipmentData *models.Equipment) *models.Equipment {
