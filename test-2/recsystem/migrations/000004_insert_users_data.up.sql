0e11728277e9bc5ac52804ed6d975a6e2fd2d68a
Aazan-Iqbal <2020152848@ub.edu.bz>
2023-05-08 15:55:49 -0600

    Corrected spl issues and renamed/reordered files to fix migration issues

---
+++ test-2/recsystem/migrations/000004_insert_users_data.up.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO "users" ("email", "first_name", "last_name", "dob", "address", "phone_number", "roles_id", "password")
+-- VALUES 
+--   ('john.doe@example.com', 'John', 'Doe', '1990-01-01', '123 Main St', '123-456-7890', 1, 'password123'),
+--   ('jane.doe@example.com', 'Jane', 'Doe', '1992-03-15', '456 Elm St', '456-789-0123', 2, 'password456'),
+--   ('bob.smith@example.com', 'Bob', 'Smith', '1985-11-25', '789 Oak Ave', '789-012-3456', 1, 'password789');
